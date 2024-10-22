b8e1b43edfbdfc9d19fc132fdc4254e61de01741
Frrend <edualfd0514@gmail.com>
2024-10-22 11:34:07 +0200

    Created FaceShape function and camera as background with two buttons.

---
+++ app/home.tsx
@@ -0,0 +1,99 @@
+import {Pressable, StyleSheet} from "react-native";
+import {HelloWave} from "@/components/HelloWave";
+import {ThemedView} from "@/components/ThemedView";
+import {ThemedText} from "@/components/ThemedText";
+import React from "react";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+
+function Button({text, iconName}: { text: string, iconName: string }) {
+    return <Pressable style={styles.button}>
+        <ThemedText style={styles.buttonText}>{text}</ThemedText>
+        <Pressable style={styles.buttonBox}>
+            <MaterialCommunityIcons name={iconName as any} size={50} color="#151718"/>
+        </Pressable>
+    </Pressable>;
+}
+
+export default function Index() {
+    return (
+        <ThemedView style={{flex: 1}}>
+            <ThemedView style={styles.containerHeader}>
+                <ThemedView style={styles.headerTextContainer}>
+                    <ThemedText style={styles.header}>Hi, Frend <HelloWave/></ThemedText>
+                    <ThemedText style={styles.underHeader}>Find the best haircuts</ThemedText>
+                </ThemedView>
+                <ThemedView style={styles.profilePicContainer}>
+                </ThemedView>
+            </ThemedView>
+            <ThemedView style={styles.container}>
+                <Button text={"Discover your face shape"} iconName={"face-man"}/>
+                <Button text={"Explore new haircuts"} iconName={"compass"}/>
+                <Button text={"Take a gamble"} iconName={"poker-chip"}/>
+            </ThemedView>
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({
+    containerHeader: {
+        display: "flex",
+        justifyContent: "space-evenly",
+        flexDirection: "row",
+        marginTop: 20,
+        backgroundColor: "#00000000"
+    },
+
+    container: {
+        flex: 1,
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        backgroundColor: "#00000000"
+    },
+
+    button: {
+        backgroundColor: "#0A7EA4",
+        width: 300,
+        height: 150,
+        borderRadius: 10
+    },
+
+    buttonText: {
+        margin: 5
+    },
+
+    buttonBox: {
+        backgroundColor: "#ECEDEE",
+        width: "100%",
+        height: 100,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    headerTextContainer: {
+        display: "flex",
+        justifyContent: "center",
+    },
+
+    header: {
+        fontSize: 25,
+    },
+
+    underHeader: {
+        fontSize: 20,
+    },
+
+    profilePicContainer: {
+        width: 90,
+        height: 90,
+        borderRadius: 1000,
+        borderColor: "#ECEDEE",
+        borderWidth: 4,
+        backgroundColor: "#0A7EA4",
+    },
+
+    profilePic: {
+        width: 100,
+    }
+})
--- app/index.tsx
+++ app/index.tsx
@@ -1,99 +1,60 @@
-import {Pressable, StyleSheet} from "react-native";
-import {HelloWave} from "@/components/HelloWave";
-import {ThemedView} from "@/components/ThemedView";
+import {Button, StyleSheet, View} from "react-native";
+import React, {useEffect, useRef, useState} from "react";
+import {Camera, CameraView} from 'expo-camera';
 import {ThemedText} from "@/components/ThemedText";
-import React from "react";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {requestPermissionsAsync} from "expo-media-library";
 
-function Button({text, iconName}: { text: string, iconName: string }) {
-    return <Pressable style={styles.button}>
-        <ThemedText style={styles.buttonText}>{text}</ThemedText>
-        <Pressable style={styles.buttonBox}>
-            <MaterialCommunityIcons name={iconName as any} size={50} color="#151718"/>
-        </Pressable>
-    </Pressable>;
-}
+export default function FaceShape() {
+    let cameraRef = useRef(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(false);
+    const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(false);
+    const [photo, setPhoto] = useState();
 
-export default function Index() {
+
+    useEffect(() => {
+        (async () => {
+            const cameraPermissions = await Camera.requestCameraPermissionsAsync();
+            const mediaLibraryPermissions = await requestPermissionsAsync();
+            setHasCameraPermission(cameraPermissions.status === "granted");
+            setHasMediaLibraryPermission(mediaLibraryPermissions.status === "granted");
+        })();
+    }, [])
+
+    if (hasCameraPermission === undefined) {
+        return <ThemedText>Requesting permissions...</ThemedText>
+    } else if (!hasCameraPermission) {
+        return <ThemedText>Permissions for camera not granted. Please change in settings.</ThemedText>
+    }
+
+    let takePic = async () => {
+        let options = {
+            quality: 1,
+            base64: true,
+            exif: false
+        }
+
+        let newPhoto = await (cameraRef.current as any).takePictureAsync(options);
+        setPhoto(newPhoto);
+    }
+
     return (
-        <ThemedView style={{flex: 1}}>
-            <ThemedView style={styles.containerHeader}>
-                <ThemedView style={styles.headerTextContainer}>
-                    <ThemedText style={styles.header}>Hi, Frend <HelloWave/></ThemedText>
-                    <ThemedText style={styles.underHeader}>Find the best haircuts</ThemedText>
-                </ThemedView>
-                <ThemedView style={styles.profilePicContainer}>
-                </ThemedView>
-            </ThemedView>
-            <ThemedView style={styles.container}>
-                <Button text={"Discover your face shape"} iconName={"face-man"}/>
-                <Button text={"Explore new haircuts"} iconName={"compass"}/>
-                <Button text={"Take a gamble"} iconName={"poker-chip"}/>
-            </ThemedView>
-        </ThemedView>
+        <CameraView style={styles.container} ref={cameraRef} facing={"front"}>
+            <View style={styles.buttonContainer}>
+                <Button title={"<"}></Button>
+                <Button title={">"}></Button>
+            </View>
+        </CameraView>
     );
 }
 
 const styles = StyleSheet.create({
-    containerHeader: {
-        display: "flex",
-        justifyContent: "space-evenly",
-        flexDirection: "row",
-        marginTop: 20,
-        backgroundColor: "#00000000"
-    },
-
     container: {
-        flex: 1,
+        flex: 1
+    },
+    buttonContainer: {
         display: "flex",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        backgroundColor: "#00000000"
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "flex-end"
     },
-
-    button: {
-        backgroundColor: "#0A7EA4",
-        width: 300,
-        height: 150,
-        borderRadius: 10
-    },
-
-    buttonText: {
-        margin: 5
-    },
-
-    buttonBox: {
-        backgroundColor: "#ECEDEE",
-        width: "100%",
-        height: 100,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-
-    headerTextContainer: {
-        display: "flex",
-        justifyContent: "center",
-    },
-
-    header: {
-        fontSize: 25,
-    },
-
-    underHeader: {
-        fontSize: 20,
-    },
-
-    profilePicContainer: {
-        width: 90,
-        height: 90,
-        borderRadius: 1000,
-        borderColor: "#ECEDEE",
-        borderWidth: 4,
-        backgroundColor: "#0A7EA4",
-    },
-
-    profilePic: {
-        width: 100,
-    }
 })
